3027dde2737021683e24a476c7c48db2b25284ef
linyuhc001 <linyuhc001@gmail.com>
2020-09-23 22:17:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 nohup caddy -conf="/etc/Caddyfile"   >/etc/caddy.txt 2>&1 &
 # Run V2Ray
-v2ray -config https://raw.githubusercontent.com/linyuhc001/wedog/master/vmess.json
+v2ray -config https://raw.githubusercontent.com/linyuhc001/weless/master/vless.json
